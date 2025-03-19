c5706462a69f2be6c6703debfecb59bfcc35a90b
Daniel <danielmcb3@gmail.com>
2025-03-19 15:51:19 +0000

    Initial commit

---
+++ src/Program.java
@@ -0,0 +1,15 @@
+package com.helloWorld;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello "+ getName("Daniel") +"!");
+
+	}
+	
+	public static String getName(String theName) {
+		return theName;
+	}
+
+}
